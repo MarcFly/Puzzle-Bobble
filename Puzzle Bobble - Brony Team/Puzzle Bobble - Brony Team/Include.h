18a919bca477d317fba05f6140cd3bd19f44d36e
MarcFly <mtorres.form@gmail.com>
2016-03-10 13:55:55 +0100

    TXT for pixels of images

---
+++ Puzzle Bobble - Brony Team/Puzzle Bobble - Brony Team/Include.h
@@ -0,0 +1,21 @@
+#ifndef __Include_H__
+#define __Include_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#include"Load_TXT.h"
+#include"Load_TXT.cpp"
+
+#include"Declare_Sound.h"
+#include"Declare_Draw.h"
+#include"Declare_Input.h"
+
+#include"Sound_Functions.cpp"
+#include"Draw_Functions.cpp"
+#include"Input_Functions.cpp"
+
+// Whatever we create
+
+#endif // __Include_H__ Inlcudes everything into the main cpp, so it lets it go to wherever needed
