683821031d259bb14dbb44b169ee1100851a7af7
Andreu997 <andreu12697@gmail.com>
2016-04-19 20:08:25 +0200

    Added all positions for  arrow sprites in the array

--- Puzzle Bobble - Brony Team/ModulePlayer.cpp
+++ Puzzle Bobble - Brony Team/ModulePlayer.cpp
@@ -20,222 +20,222 @@ ModulePlayer::ModulePlayer()
 	arrow[0].x = 15;
 	arrow[0].y = 514;
 	arrow[0].w = 22;
-	arrow[0].h = 55;
+	arrow[0].h = 54;
 
-	arrow[1].x = 14;
-	arrow[1].y = 514;
-	arrow[1].w = 24;
-	arrow[1].h = 55;
+	arrow[1].x = 49;
+	arrow[1].y = 515;
+	arrow[1].w = 21;
+	arrow[1].h = 54;
 
-	arrow[2].x = 14;
-	arrow[2].y = 514;
-	arrow[2].w = 24;
-	arrow[2].h = 55;
+	arrow[2].x = 83;
+	arrow[2].y = 515;
+	arrow[2].w = 21;
+	arrow[2].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[3].x = 117;
+	arrow[3].y = 515;
+	arrow[3].w = 21;
+	arrow[3].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[4].x = 151;
+	arrow[4].y = 515;
+	arrow[4].w = 21;
+	arrow[4].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[5].x = 185;
+	arrow[5].y = 515;
+	arrow[5].w = 21;
+	arrow[5].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[6].x = 219;
+	arrow[6].y = 515;
+	arrow[6].w = 21;
+	arrow[6].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[7].x = 253;
+	arrow[7].y = 516;
+	arrow[7].w = 21;
+	arrow[7].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[8].x = 287;
+	arrow[8].y = 515;
+	arrow[8].w = 21;
+	arrow[8].h = 53;
 
-	arrow[9].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[9].x = 321;
+	arrow[9].y = 516;
+	arrow[9].w = 21;
+	arrow[9].h = 54;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[10].x = 355;
+	arrow[10].y = 516;
+	arrow[10].w = 14;
+	arrow[10].h = 53;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[11].x = 389;
+	arrow[11].y = 516;
+	arrow[11].w = 14;
+	arrow[11].h = 53;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[12].x = 423;
+	arrow[12].y = 516;
+	arrow[12].w = 14;
+	arrow[12].h = 53;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[13].x = 456;
+	arrow[13].y = 517;
+	arrow[13].w = 23;
+	arrow[13].h = 53;
 
-	arrow[14].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[14].x = 490;
+	arrow[14].y = 517;
+	arrow[14].w = 23;
+	arrow[14].h = 52;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[15].x = 523;
+	arrow[15].y = 517;
+	arrow[15].w = 25;
+	arrow[15].h = 52;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[16].x = 557;
+	arrow[16].y = 517;
+	arrow[16].w = 26;
+	arrow[16].h = 52;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[17].x = 591;
+	arrow[17].y = 517;
+	arrow[17].w = 26;
+	arrow[17].h = 52;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[18].x = 12;
+	arrow[18].y = 584;
+	arrow[18].w = 28;
+	arrow[18].h = 50;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[19].x = 46;
+	arrow[19].y = 584;
+	arrow[19].w = 29;
+	arrow[19].h = 50;
 
-	arrow[20].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[20].x = 79;
+	arrow[20].y = 584;
+	arrow[20].w = 30;
+	arrow[20].h = 50;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[21].x = 113;
+	arrow[21].y = 585;
+	arrow[21].w = 31;
+	arrow[21].h = 49;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[22].x = 147;
+	arrow[22].y = 585;
+	arrow[22].w = 32;
+	arrow[22].h = 49;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[23].x = 196;
+	arrow[23].y = 586;
+	arrow[23].w = 33;
+	arrow[23].h = 48;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[24].x = 246;
+	arrow[24].y = 586;
+	arrow[24].w = 34;
+	arrow[24].h = 48;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[25].x = 295;
+	arrow[25].y = 586;
+	arrow[25].w = 35;
+	arrow[25].h = 47;
 
-	arrow[26].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[26].x = 345;
+	arrow[26].y = 586;
+	arrow[26].w = 36;
+	arrow[26].h = 47;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[27].x = 395;
+	arrow[27].y = 587;
+	arrow[27].w = 37;
+	arrow[27].h = 46;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[28].x = 444;
+	arrow[28].y = 588;
+	arrow[28].w = 38;
+	arrow[28].h = 44;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[29].x = 494;
+	arrow[29].y = 588;
+	arrow[29].w = 38;
+	arrow[29].h = 44;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[30].x = 544;
+	arrow[30].y = 588;
+	arrow[30].w = 39;
+	arrow[30].h = 44;
+	
+	arrow[31].x = 23;
+	arrow[31].y = 655;
+	arrow[31].w = 41;
+	arrow[31].h = 43;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[32].x = 73;
+	arrow[32].y = 656;
+	arrow[32].w = 42;
+	arrow[32].h = 41;
 
-	arrow[32].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[33].x = 123;
+	arrow[33].y = 656;
+	arrow[33].w = 42;
+	arrow[33].h = 41;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[34].x = 173;
+	arrow[34].y = 656;
+	arrow[34].w = 42;
+	arrow[34].h = 41;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[35].x = 222;
+	arrow[35].y = 657;
+	arrow[35].w = 44;
+	arrow[35].h = 40;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[36].x = 272;
+	arrow[36].y = 658;
+	arrow[36].w = 44;
+	arrow[36].h = 38;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[37].x = 322;
+	arrow[37].y = 659;
+	arrow[37].w = 45;
+	arrow[37].h = 37;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[38].x = 372;
+	arrow[38].y = 659;
+	arrow[38].w = 45;
+	arrow[38].h = 37;
 
-	arrow[38].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[39].x = 421;
+	arrow[39].y = 659;
+	arrow[39].w = 47;
+	arrow[39].h = 36;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[40].x = 472;
+	arrow[40].y = 660;
+	arrow[40].w = 47;
+	arrow[40].h = 35;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[41].x = 524;
+	arrow[41].y = 661;
+	arrow[41].w = 48;
+	arrow[41].h = 33;
+	
+	arrow[42].x = 21;
+	arrow[42].y = 712;
+	arrow[42].w = 48;
+	arrow[42].h = 32;
 
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
-
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
-
-	arrow[3].x = 14;
-	arrow[3].y = 514;
-	arrow[3].w = 24;
-	arrow[3].h = 55;
+	arrow[43].x = 85;
+	arrow[43].y = 712;
+	arrow[43].w = 48;
+	arrow[43].h = 32;
 
 	arrow[44].x = 149;
 	arrow[44].y = 713;
