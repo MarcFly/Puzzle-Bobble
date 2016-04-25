73fae7d7fb02dbf34089fe850668190efde3d9fd
MarcFly <mtorres.form@gmail.com>
2016-04-25 12:26:40 +0200

    Colors and Animations in idle state is done

--- Puzzle Bobble - Brony Team/Bobbles.cpp
+++ Puzzle Bobble - Brony Team/Bobbles.cpp
@@ -5,6 +5,10 @@
 Bobble_Blue::Bobble_Blue(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 12, 260, 16, 16 });
+	idle.PushBack({ 32, 260, 16, 16 });
+	idle.PushBack({ 52, 260, 16, 16 });
+	idle.PushBack({ 72, 260, 16, 16 });
+	idle.PushBack({ 92, 260, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -17,6 +21,10 @@ Bobble_Blue::Bobble_Blue(int x, int y) : Enemy(x, y)
 Bobble_Red::Bobble_Red(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 12, 312, 16, 16 });
+	idle.PushBack({ 12, 312, 16, 16 });
+	idle.PushBack({ 32, 312, 16, 16 });
+	idle.PushBack({ 32, 312, 16, 16 });
+	idle.PushBack({ 92, 312, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -29,6 +37,10 @@ Bobble_Red::Bobble_Red(int x, int y) : Enemy(x, y)
 Bobble_Green::Bobble_Green(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 318, 260, 16, 16 });
+	idle.PushBack({ 338, 260, 16, 16 });
+	idle.PushBack({ 358, 260, 16, 16 });
+	idle.PushBack({ 378, 260, 16, 16 });
+	idle.PushBack({ 398, 260, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -41,6 +53,10 @@ Bobble_Green::Bobble_Green(int x, int y) : Enemy(x, y)
 Bobble_Yellow::Bobble_Yellow(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 12, 338, 16, 16 });
+	idle.PushBack({ 32, 338, 16, 16 });
+	idle.PushBack({ 52, 338, 16, 16 });
+	idle.PushBack({ 72, 338, 16, 16 });
+	idle.PushBack({ 92, 338, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -53,6 +69,10 @@ Bobble_Yellow::Bobble_Yellow(int x, int y) : Enemy(x, y)
 Bobble_Black::Bobble_Black(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 318, 286, 16, 16 });
+	idle.PushBack({ 338, 286, 16, 16 });
+	idle.PushBack({ 358, 286, 16, 16 });
+	idle.PushBack({ 378, 286, 16, 16 });
+	idle.PushBack({ 398, 286, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -65,6 +85,10 @@ Bobble_Black::Bobble_Black(int x, int y) : Enemy(x, y)
 Bobble_Orange::Bobble_Orange(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 318, 312, 16, 16 });
+	idle.PushBack({ 338, 312, 16, 16 });
+	idle.PushBack({ 358, 312, 16, 16 });
+	idle.PushBack({ 378, 312, 16, 16 });
+	idle.PushBack({ 398, 312, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -77,6 +101,10 @@ Bobble_Orange::Bobble_Orange(int x, int y) : Enemy(x, y)
 Bobble_Grey::Bobble_Grey(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 12, 286, 16, 16 });
+	idle.PushBack({ 32, 286, 16, 16 });
+	idle.PushBack({ 52, 286, 16, 16 });
+	idle.PushBack({ 72, 286, 16, 16 });
+	idle.PushBack({ 92, 286, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
@@ -89,6 +117,10 @@ Bobble_Grey::Bobble_Grey(int x, int y) : Enemy(x, y)
 Bobble_Purple::Bobble_Purple(int x, int y) : Enemy(x, y)
 {
 	idle.PushBack({ 318, 338, 16, 16 });
+	idle.PushBack({ 338, 338, 16, 16 });
+	idle.PushBack({ 358, 338, 16, 16 });
+	idle.PushBack({ 378, 338, 16, 16 });
+	idle.PushBack({ 398, 338, 16, 16 });
 	idle.speed = 0.05;
 
 	animation = &idle;
