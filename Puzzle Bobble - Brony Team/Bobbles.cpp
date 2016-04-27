fc7754c286bfa9490070b4e8d6b4c2f8c758a676
Andreu997 <andreu12697@gmail.com>
2016-04-27 14:56:29 +0200

    Changed bubble sprites for enemy

--- Puzzle Bobble - Brony Team/Bobbles.cpp
+++ Puzzle Bobble - Brony Team/Bobbles.cpp
@@ -4,7 +4,7 @@
 
 Bobble_Blue::Bobble_Blue(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 12, 260, 16, 16 });
+	idle.PushBack({ 18, 559, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 112, 260, 16, 16 });
@@ -25,7 +25,7 @@ Bobble_Blue::Bobble_Blue(int x, int y) : Enemy(x, y)
 
 Bobble_Red::Bobble_Red(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 12, 312, 16, 16 });
+	idle.PushBack({ 18, 607, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 112, 312, 16, 16 });
@@ -43,7 +43,7 @@ Bobble_Red::Bobble_Red(int x, int y) : Enemy(x, y)
 
 Bobble_Green::Bobble_Green(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 318, 260, 16, 16 });
+	idle.PushBack({ 266, 559, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 338, 260, 16, 16 });
@@ -61,7 +61,7 @@ Bobble_Green::Bobble_Green(int x, int y) : Enemy(x, y)
 
 Bobble_Yellow::Bobble_Yellow(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 12, 338, 16, 16 });
+	idle.PushBack({ 18, 631, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 32, 338, 16, 16 });
@@ -79,7 +79,7 @@ Bobble_Yellow::Bobble_Yellow(int x, int y) : Enemy(x, y)
 
 Bobble_Black::Bobble_Black(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 318, 286, 16, 16 });
+	idle.PushBack({ 266, 583, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 338, 286, 16, 16 });
@@ -97,7 +97,7 @@ Bobble_Black::Bobble_Black(int x, int y) : Enemy(x, y)
 
 Bobble_Orange::Bobble_Orange(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 318, 312, 16, 16 });
+	idle.PushBack({ 266, 607, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 338, 312, 16, 16 });
@@ -115,7 +115,7 @@ Bobble_Orange::Bobble_Orange(int x, int y) : Enemy(x, y)
 
 Bobble_Grey::Bobble_Grey(int x, int y) : Enemy(x, y)
 {
-	idle.PushBack({ 12, 286, 16, 16 });
+	idle.PushBack({ 18, 583, 16, 16 });
 	idle.speed = 0.05f;
 
 	explode.PushBack({ 32, 286, 16, 16 });
@@ -136,7 +136,7 @@ Bobble_Purple::Bobble_Purple(int x, int y) : Enemy(x, y)
 	idle.PushBack({ 318, 338, 16, 16 });
 	idle.speed = 0.05f;
 
-	explode.PushBack({ 338, 338, 16, 16 });
+	explode.PushBack({ 266, 631, 16, 16 });
 	explode.PushBack({ 358, 338, 16, 16 });
 	explode.PushBack({ 378, 338, 16, 16 });
 	explode.PushBack({ 398, 338, 16, 16 });
