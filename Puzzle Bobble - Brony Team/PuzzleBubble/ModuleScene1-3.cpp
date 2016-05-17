bcb2816ddc9b2c7ef26d77fa0b498544b85aa194
Andreu997 <andreu12697@gmail.com>
2016-05-17 13:14:37 +0200

    added bubbles to erase

--- Puzzle Bobble - Brony Team/PuzzleBubble/ModuleScene1-3.cpp
+++ Puzzle Bobble - Brony Team/PuzzleBubble/ModuleScene1-3.cpp
@@ -306,7 +306,24 @@ update_status ModuleScene1to3::PostUpdate(){
 							else
 								App->enemies->AddEnemy(BOBBLE_YELLOW, ((x + 1) * 16) + BUBBLE_OFFSET_X_ODD, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
 							break;
-						
+						case g:
+							if (y % 2)
+								App->enemies->AddEnemy(BOBBLE_GREY, ((x + 1) * 16) + BUBBLE_OFFSET_X_PAIR, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
+							else
+								App->enemies->AddEnemy(BOBBLE_GREY, ((x + 1) * 16) + BUBBLE_OFFSET_X_ODD, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
+							break;
+						case V:
+							if (y % 2)
+								App->enemies->AddEnemy(BOBBLE_BLACK, ((x + 1) * 16) + BUBBLE_OFFSET_X_PAIR, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
+							else
+								App->enemies->AddEnemy(BOBBLE_BLACK, ((x + 1) * 16) + BUBBLE_OFFSET_X_ODD, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
+							break;
+						case P:
+							if (y % 2)
+								App->enemies->AddEnemy(BOBBLE_PURPLE, ((x + 1) * 16) + BUBBLE_OFFSET_X_PAIR, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
+							else
+								App->enemies->AddEnemy(BOBBLE_PURPLE, ((x + 1) * 16) + BUBBLE_OFFSET_X_ODD, ((y + 1) * 15) + BUBBLE_OFFSET_Y);
+							break;
 					}
 				}
 			}
